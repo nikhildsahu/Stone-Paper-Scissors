cd3daa1f8d46b4d1148e22b0cf71dbb604ec417a
stanleeniko <stanleeniko@gmail.com>
2019-04-06 23:46:11 +0530

    blah

---
+++ src/Components/Main.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {
+  Grid,
+  Typography,
+  Card,
+  TextField,
+  Button,
+  Avatar
+} from "@material-ui/core";
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Grid container>
+          <Grid item md={6} style={{ backgroundColor: black }} />
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Main;
