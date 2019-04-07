80e6e975e092936dc400dd7d42684915add89681
stanleeniko <stanleeniko@gmail.com>
2019-04-08 01:17:41 +0530

    Done V1.0

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,27 +4,146 @@ import paperimg from "../img/paper.png";
 import stoneimg from "../img/stone.png";
 import scissors from "../img/scissors.png";
 import { ScrollTo } from "react-scroll-to";
+import avatarimg from "../img/avatar.png";
+import { Delay } from "react-delay";
 import {
   Grid,
   Typography,
   Card,
   TextField,
   Button,
-  Avatar
+  Avatar,
+  Paper,
+  Fab
 } from "@material-ui/core";
+import SelectInput from "@material-ui/core/Select/SelectInput";
 class Main extends Component {
   state = {
-    name: ""
+    name: "",
+    compscore: 0,
+    playerscore: 0,
+    nameset: "",
+    userchoice: "",
+    userchoiceNO: "",
+    userchoicename: "",
+    compchoice: "",
+    compchoiceNO: "",
+    compchoicename: "",
+    timer: "",
+    timeRemainingInSeconds: "",
+    result: "",
+    block: 1,
+    display: 1
+  };
+  setPlayer = c => {
+    if (c == 1) {
+      this.setState({ userchoice: require("../img/stone.png") });
+      this.setState({ userchoicename: "Stone" });
+      this.setState({ userchoiceNO: 1 });
+    } else if (c == 2) {
+      this.setState({ userchoice: require("../img/paper.png") });
+      this.setState({ userchoicename: "Paper" });
+      this.setState({ userchoiceNO: 2 });
+    } else if (c == 3) {
+      this.setState({ userchoice: require("../img/scissors.png") });
+      this.setState({ userchoicename: "Scissor" });
+      this.setState({ userchoiceNO: 3 });
+    }
   };
 
   handleChange = name => event => {
     this.setState({
       [name]: event.target.value
     });
+    this.setState({ nameset: 1 });
   };
+  go = () => {
+    this.setState({ timeRemainingInSeconds: 3 });
+    this.state.timer = setInterval(() => {
+      this.decrementTimeRemaining();
+    }, 1000);
+
+    // setTimeout(
+    //   function() {
+    //     this.setState({ timer: this.state.timer - 1 });
+    //   }.bind(this),
+    //   1000
+    // );
+  };
+  decrementTimeRemaining = () => {
+    if (this.state.timeRemainingInSeconds > 0) {
+      this.setState({
+        timeRemainingInSeconds: this.state.timeRemainingInSeconds - 1
+      });
+    } else {
+      clearInterval(this.state.timer);
+      this.generateCompChoice();
+    }
+  };
+  generateCompChoice = () => {
+    var c = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    console.log(c);
+    if (c == 1) {
+      this.setState({ compchoice: require("../img/stone.png") });
+      this.setState({ compchoicename: "Stone" });
+      this.setState({ compchoiceNO: 1 });
+    } else if (c == 2) {
+      this.setState({ compchoice: require("../img/paper.png") });
+      this.setState({ compchoicename: "Paper" });
+      this.setState({ compchoiceNO: 2 });
+    } else if (c == 3) {
+      this.setState({ compchoice: require("../img/scissors.png") });
+      this.setState({ compchoicename: "Scissor" });
+      this.setState({ compchoiceNO: 3 });
+    }
+    //   this.showResult.bind(this);
+    // };
+    // showResult = () => {
+    this.setState({ block: "" });
+    var p = this.state.userchoiceNO;
+    var c = this.state.compchoiceNO;
+    var w = "You Win";
+    var l = "You lose";
+    console.log(p + "" + c);
+    if (p != c) {
+      if (p == 1) {
+        if (c == 2) {
+          this.setState({ compscore: this.state.compscore + 1 });
+          this.setState({ result: l });
+        } else if (c == 3) {
+          this.setState({ playerscore: this.state.playerscore + 1 });
+          this.setState({ result: w });
+        }
+      } else if (p == 2) {
+        if (c == 3) {
+          this.setState({ compscore: this.state.compscore + 1 });
+          this.setState({ result: l });
+        } else if (c == 1) {
+          this.setState({ playerscore: this.state.playerscore + 1 });
+          this.setState({ result: w });
+        }
+      } else if (p == 3) {
+        if (c == 1) {
+          this.setState({ compscore: this.state.compscore + 1 });
+          this.setState({ result: l });
+        } else if (c == 2) {
+          this.setState({ playerscore: this.state.playerscore + 1 });
+          this.setState({ result: w });
+        }
+      }
+    } else {
+      this.setState({ result: "oops, Tie" });
+    }
+  };
+  clearAll() {
+    this.setState({ userchoice: "" });
+    this.setState({ compchoice: "" });
+    this.setState({ result: "" });
+    this.setState({ block: 1 });
+  }
   render() {
     return (
-      <div style={{ backgroundColor: "#C2185B", height: "1050px" }}>
+      <div style={{ backgroundColor: "#C2185B", height: "1500px" }}>
         {/* <h1>jdgdgg</h1> */}
         <Grid container justify="center">
           <Grid item md={6}>
@@ -41,18 +160,25 @@ class Main extends Component {
               gutterBottom
               style={{ textAlign: "center", color: "white" }}
             >
-              <Typing loop speed={150}>
-                <span>Hey There!!</span>
+              {""}
+
+              <Typing speed={170}>
+                <Typing.Reset count={1} delay={500} />
+                <span>Hey There!! </span>
 
                 <Typing.Backspace count={12} />
-                <span>Welcome</span>
+
+                <span>Welcome </span>
                 <Typing.Backspace count={8} />
+
                 <span>Let's Play</span>
-                <Typing.Backspace count={11} />
+                <Typing.Backspace count={10} />
               </Typing>
             </Typography>
           </Grid>
-          <Grid item md={12} />
+          <Grid item md={12} style={{ height: "100px" }}>
+            {}
+          </Grid>
           <Grid item md={4} style={{ alignContent: "center", height: "300px" }}>
             <img
               src={stoneimg}
@@ -144,14 +270,253 @@ class Main extends Component {
             >
               <ScrollTo>
                 {({ scrollTo }) => (
-                  <a onClick={() => scrollTo({ y: 600, smooth: true })}>
+                  <a onClick={() => scrollTo({ y: 800, smooth: true })}>
                     Let's Go!
                   </a>
                 )}
               </ScrollTo>
             </Button>
           </Grid>
+          <Grid item md={12} style={{ height: "50px" }}>
+            {}
+          </Grid>
         </Grid>
+        {this.state.nameset ? (
+          <div>
+            <div style={{ backgroundColor: "#F48FB1" }}>
+              <Grid container justify="center">
+                {/* <Grid item md={4} /> */}
+                <Grid item md={4}>
+                  <Typography
+                    variant="h6"
+                    gutterBottom
+                    style={{ textAlign: "center" }}
+                  >
+                    Score
+                  </Typography>
+                </Grid>
+                <Grid item md={12} />
+                <Grid item md={6}>
+                  <Typography
+                    variant="h6"
+                    gutterBottom
+                    style={{ textAlign: "center" }}
+                  >
+                    {this.state.name}:{this.state.playerscore}
+                  </Typography>
+                </Grid>
+
+                <Grid item md={6}>
+                  <Typography
+                    variant="h6"
+                    gutterBottom
+                    style={{ textAlign: "center", paddingRight: "160px" }}
+                  >
+                    Computer:{this.state.compscore}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </div>
+            <Grid container>
+              <Grid item md={12} style={{ height: "60px" }}>
+                {}
+              </Grid>
+              <Grid item md={2} />
+              <Grid item md={3}>
+                <Paper
+                  elevation={1}
+                  style={{ height: "300px", width: "300px" }}
+                >
+                  {this.state.userchoice ? (
+                    <div>
+                      <Typography
+                        variant="button"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        Player Choice
+                      </Typography>
+                      <img
+                        src={this.state.userchoice}
+                        alt=""
+                        style={{ paddingLeft: "50px" }}
+                      />
+                      <Typography
+                        variant="button"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        {this.state.userchoicename}
+                      </Typography>
+                    </div>
+                  ) : (
+                    <div>
+                      <Typography
+                        variant="h6"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        Choose
+                      </Typography>
+                      <br />
+                      <br />
+                      <Typography
+                        variant="h6"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        Your Turn first
+                      </Typography>
+                    </div>
+                  )}
+                </Paper>
+              </Grid>
+              <Grid item md={2}>
+                {this.state.userchoice && this.state.block ? (
+                  <Fab
+                    variant="extended"
+                    aria-label="Delete"
+                    style={{
+                      position: "relative",
+                      left: "50px",
+                      top: "150px"
+                    }}
+                    onClick={this.go.bind(this)}
+                  >
+                    Go
+                  </Fab>
+                ) : null}
+                ;
+              </Grid>
+
+              <Grid item md={3}>
+                <Paper
+                  elevation={1}
+                  style={{ height: "300px", width: "300px" }}
+                >
+                  {this.state.compchoice ? (
+                    <div>
+                      <Typography
+                        variant="button"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        Computer Choice
+                      </Typography>
+                      <img
+                        src={this.state.compchoice}
+                        alt=""
+                        style={{ paddingLeft: "50px" }}
+                      />
+                      <Typography
+                        variant="button"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        {this.state.compchoicename}
+                      </Typography>
+                    </div>
+                  ) : (
+                    <div>
+                      <div style={{ height: "50px" }} />
+
+                      <Typography
+                        variant="h6"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        Waiting For You
+                      </Typography>
+                      <br />
+                      <Typography
+                        component="h2"
+                        variant="display3"
+                        gutterBottom
+                        style={{ textAlign: "center" }}
+                      >
+                        {this.state.timeRemainingInSeconds}
+                      </Typography>
+                    </div>
+                  )}
+                </Paper>
+              </Grid>
+              <Grid item md={2} />
+              <Grid item md={12} style={{ height: "30px" }}>
+                {}
+              </Grid>
+              <Grid item md={2} />
+              <Grid item md={3}>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  style={{ color: "white", position: "relative", left: "20px" }}
+                  onClick={this.setPlayer.bind(this, 1)}
+                >
+                  Stone
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  onClick={this.setPlayer.bind(this, 2)}
+                  style={{ color: "white", position: "relative", left: "30px" }}
+                >
+                  Paper
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  style={{ color: "white", position: "relative", left: "40px" }}
+                  onClick={this.setPlayer.bind(this, 3)}
+                >
+                  Scissor
+                </Button>
+              </Grid>
+              <Grid item md={2} />
+              <Grid item md={3}>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  style={{ color: "white", width: "300px" }}
+                >
+                  Computer
+                </Button>
+              </Grid>
+              <Grid item md={2} />
+              <Grid item md={12}>
+                <Typography
+                  component="h2"
+                  variant="display3"
+                  gutterBottom
+                  style={{ textAlign: "center", paddingRight: "100px" }}
+                >
+                  {this.state.result}
+                </Typography>
+              </Grid>
+              <Grid item md={5} />
+              <Grid item md={4}>
+                {this.state.result ? (
+                  <Button
+                    variant="outlined"
+                    color="inherit"
+                    style={{
+                      color: "white",
+                      position: "relative",
+                      left: "20px"
+                    }}
+                    onClick={this.clearAll.bind(this)}
+                  >
+                    Try ,Again
+                  </Button>
+                ) : null}
+                ;
+              </Grid>
+            </Grid>
+          </div>
+        ) : (
+          <h5 style={{ textAlign: "center", color: "white" }}>
+            Enter Your Name To Play Game
+          </h5>
+        )}
       </div>
     );
   }
